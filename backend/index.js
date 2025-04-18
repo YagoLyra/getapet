6b928131f1b9b653c7c66ac20f71d3637119e2ea
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-18 18:11:24 -0300

    Configura rotas de usuários no servidor: - Importa UserRoutes do arquivo routes/userRoutes.js - Adiciona prefixo /users para todas as rotas de usuários

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,9 @@ app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.static("public"));
 
 // Routes
+const UserRoutes = require("./routes/userRoutes");
+
+app.use("/users", UserRoutes);
 
 app.listen(5000, () => {
   console.log("Servidor rodando na porta 5000");
