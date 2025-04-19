474eef7c09ce6aff21ecb7a08c9d182931410c0d
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-19 13:58:57 -0300

    Implementa helper image-upload: - Configura armazenamento de imagens com multer, organizando-as em pastas específicas (users e pets) com base na URL base - Define nomes únicos para os arquivos usando timestamps - Adiciona filtro para aceitar apenas arquivos nos formatos jpg, jpeg e png - Retorna mensagens de erro para formatos de arquivo inválidos

---
+++ backend/helpers/image-upload.js
@@ -0,0 +1,30 @@
+const multer = require("multer");
+const path = require("path");
+
+const imageStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    let folder = "";
+    if (req.baseUrl.includes("users")) {
+      folder = "users";
+    } else if (req.baseUrl.includes("pets")) {
+      folder = "pets";
+    }
+    cb(null, `public/images/${folder}`);
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const imageUpload = multer({
+  storage: imageStorage,
+  fileFilter: (req, file, cb) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Por favor, envie apenas jpg, jpeg ou png! "), false);
+    }
+    cb(undefined, true);
+  },
+});
+
+module.exports = { imageUpload };
