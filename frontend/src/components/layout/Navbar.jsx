f3393f7eefb867238d103c9a0d5369cdb30c664f
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-22 19:58:04 -0300

    Atualiza o componente Navbar: - Importa Link do react-router-dom para navegação - Importa e aplica estilos do arquivo Navbar.module.css - Adiciona logo e título "Get A Pet" no cabeçalho - Configura links para Adotar, Entrar e Cadastrar - Adiciona funcionalidade de logout para usuários autenticados

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,10 +1,20 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+
+// styles
+import styles from "./Navbar.module.css";
+
 import Logo from "../../assets/img/logo.png";
 
+// context
+import { Context } from "../../context/UserContext";
+
 const Navbar = () => {
+  const { authenticated, logout } = useContext(Context);
+
   return (
-    <nav>
-      <div>
+    <nav className={styles.navbar}>
+      <div className={styles.navbar_logo}>
         <img src={Logo} alt="Get A Pet" />
         <h2>Get A Pet</h2>
       </div>
@@ -12,12 +22,20 @@ const Navbar = () => {
         <li>
           <Link to="/">Adotar</Link>
         </li>
-        <li>
-          <Link to="/login">Entrar</Link>
-        </li>
-        <li>
-          <Link to="/register">Cadastrar</Link>
-        </li>
+        {authenticated ? (
+          <>
+            <li onClick={logout}>Sair</li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Entrar</Link>
+            </li>
+            <li>
+              <Link to="/register">Cadastrar</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
