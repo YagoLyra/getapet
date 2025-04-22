7ed181415a9f4d44815d5e42a835621c098faad6
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-22 19:59:10 -0300

    Cria componente Register: - Importa Input para campos do formulário e aplica estilos do arquivo Form.module.css - Configura estado local para armazenar dados do usuário - Implementa funções handleChange e handleSubmit para manipular eventos do formulário - Adiciona campos de entrada para nome, telefone, email, senha e confirmação de senha - Inclui link para redirecionar usuários já registrados para a página de login

--- frontend/src/components/pages/auth/Register.jsx
+++ frontend/src/components/pages/auth/Register.jsx
@@ -1,7 +1,71 @@
+import { useState, useContext } from "react";
+
+import Input from "../../form/input";
+import { Link } from "react-router-dom";
+
+import styles from "../../form/Form.module.css";
+
+// context
+import { Context } from "../../../context/UserContext";
+
 const Register = () => {
+  const [user, setUser] = useState({});
+  const { register } = useContext(Context);
+
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    register(user);
+  }
+
   return (
-    <section>
-      <h1>Register</h1>
+    <section className={styles.form_container}>
+      <h1>Registrar</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite seu nome"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite seu telefone"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite seu email"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Confirmar Senha"
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem uma conta? <Link to="/login">Clique aqui</Link> para fazer login.
+      </p>
     </section>
   );
 };
