877b7e541c28b2a8008e681863df33315849d5e9
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-23 14:04:34 -0300

    Cria componente Login: - Importa Input para campos do formulário e aplica estilos do arquivo Form.module.css - Configura estado local para armazenar dados do usuário - Implementa funções handleChange e handleSubmit para manipular eventos do formulário - Adiciona campos de entrada para email e senha - Inclui link para redirecionar usuários sem conta para a página de registro

--- frontend/src/components/pages/auth/Login.jsx
+++ frontend/src/components/pages/auth/Login.jsx
@@ -1,7 +1,49 @@
+import { useState, useContext } from "react";
+import Input from "../../form/Input";
+
+import styles from "../../form/Form.module.css";
+
+import { Link } from "react-router-dom";
+
+// context
+import { Context } from "../../../context/UserContext";
+
 const Login = () => {
+  const [user, setUser] = useState({});
+  const { login } = useContext(Context);
+
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await login(user);
+  }
+
   return (
-    <section>
+    <section className={styles.form_container}>
       <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite seu email"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>
+        Não tem uma conta? <Link to="/register">Cadastre-se</Link>
+      </p>
     </section>
   );
 };
