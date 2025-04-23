7afb24c41fef83b3761ddc22cd0947238b6fa389
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-23 14:06:36 -0300

    Atualiza hook useAuth: - Implementa função login para autenticar usuários e exibir mensagens de sucesso ou erro - Adiciona função logout para limpar autenticação, remover token e redirecionar para a página inicial - Integra mensagens de feedback com setFlashMessage

--- frontend/src/hooks/useAuth.jsx
+++ frontend/src/hooks/useAuth.jsx
@@ -44,6 +44,23 @@ export default function useAuth() {
     history.push("/");
   }
 
+  async function login(user) {
+    let msgText = "Login realizado com sucesso!";
+    let msgType = "success";
+
+    try {
+      const data = await api.post("/users/login", user).then((response) => {
+        return response.data;
+      });
+      await authUser(data);
+    } catch (error) {
+      msgText = error.response.data.message;
+      msgType = "error";
+    }
+
+    setFlashMessage(msgText, msgType);
+  }
+
   async function logout() {
     const msgText = "Logout realizado com sucesso!";
     const msgType = "success";
@@ -56,5 +73,5 @@ export default function useAuth() {
     setFlashMessage(msgText, msgType);
   }
 
-  return { authenticated, register, logout };
+  return { authenticated, register, logout, login };
 }
