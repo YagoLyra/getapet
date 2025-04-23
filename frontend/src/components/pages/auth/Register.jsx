ca27e5eb419d12dbab80aecb57ce85d7d6afcf3f
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-23 14:05:00 -0300

    Ajuste de import

--- frontend/src/components/pages/auth/Register.jsx
+++ frontend/src/components/pages/auth/Register.jsx
@@ -1,6 +1,6 @@
 import { useState, useContext } from "react";
 
-import Input from "../../form/input";
+import Input from "../../form/Input";
 import { Link } from "react-router-dom";
 
 import styles from "../../form/Form.module.css";
