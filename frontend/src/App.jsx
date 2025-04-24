0ff65caa9e9499efd2e92f85dd23102d980c0a8e
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-23 21:31:18 -0300

    Atualiza o componente App.jsx: - Configura rota "/pet/mypets" para exibir os pets do usuário - Configura rota "/pet/add" para adicionar um novo pet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ import Login from "./components/pages/auth/Login";
 import Register from "./components/pages/auth/Register";
 import Home from "./components/pages/Home";
 import Profile from "./components/pages/user/Profile";
+import MyPets from "./components/pages/Pet/MyPets";
+import AddPet from "./components/pages/Pet/AddPet";
 
 // context
 import { UserProvider } from "./context/UserContext";
@@ -31,6 +33,12 @@ function App() {
             </Route>
             <Route path="/user/profile">
               <Profile />
+            </Route>
+            <Route path="/pet/mypets">
+              <MyPets />
+            </Route>
+            <Route path="/pet/add">
+              <AddPet />
             </Route>
             <Route path="/">
               <Home />
