542c71ac068f313c857cff6ec13f7461f10add6d
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-23 21:34:29 -0300

    Cria componente MyPets: - Implementa listagem de pets cadastrados pelo usuário com dados obtidos da API - Adiciona botão para cadastrar novos pets com link para a página de cadastro - Exibe imagem, nome e ações disponíveis para cada pet, como editar ou excluir - Integra mensagens de feedback com useFlashMessage - Exibe mensagem caso o usuário não tenha pets cadastrados

---
+++ frontend/src/components/pages/Pet/MyPets.jsx
@@ -0,0 +1,68 @@
+import api from "../../../utils/api";
+
+import styles from "./Dashboard.module.css";
+
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import RoundedImage from "../../layout/RoundedImage";
+
+import useFlashMessage from "../../../hooks/useFlashMessage";
+
+const MyPets = () => {
+  const [pets, setPets] = useState([]);
+  const [token] = useState(localStorage.getItem("token") || "");
+  const { setFlashMessage } = useFlashMessage();
+
+  useEffect(() => {
+    api
+      .get("/pets/mypets", {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => {
+        setPets(response.data.pets);
+      });
+  }, [token]);
+
+  return (
+    <section>
+      <div className={styles.petlist_header}>
+        <h1>MyPets</h1>
+        <Link to="/pet/add">Cadastrar Pet</Link>
+      </div>
+      <div className={styles.petlist_container}>
+        {pets.length > 0 &&
+          pets.map((pet) => (
+            <div className={styles.petlist_row} key={pet._id}>
+              <RoundedImage
+                src={`${import.meta.env.VITE_API}/images/pets/${pet.images[0]}`}
+                alt={pet.name}
+                width="px75"
+              />
+              <span className="bold">{pet.name}</span>
+              <div className={styles.actions}>
+                {pet.available ? (
+                  <>
+                    {pet.adopted && (
+                      <button className={styles.conclude_btn}>
+                        Concluir adoção
+                      </button>
+                    )}
+                    <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
+                    <button>Excluir</button>
+                  </>
+                ) : (
+                  <p>Pet já adotado</p>
+                )}
+              </div>
+            </div>
+          ))}
+        {pets.length === 0 && <p>Você ainda não tem pets cadastrados</p>}
+      </div>
+    </section>
+  );
+};
+
+export default MyPets;
