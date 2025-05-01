fca3c647179e23bb959702f9a69e2b485023e8e5
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-30 21:35:10 -0300

    Atualiza o componente Home: - Importa dependências necessárias, incluindo api, Link, useState, useEffect e estilos do arquivo Home.module.css - Implementa chamada à API para buscar pets disponíveis e armazena os dados no estado - Renderiza lista de pets com informações como nome, peso e status de disponibilidade - Adiciona link para detalhes do pet ou exibe mensagem de "Adotado!" caso não esteja disponível

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,7 +1,50 @@
+import api from "../../utils/api";
+
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import styles from "./Home.module.css";
+
 const Home = () => {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    api.get("/pets").then((response) => {
+      setPets(response.data.pets);
+    });
+  }, []);
+
   return (
     <section>
-      <h1>Home</h1>
+      <div className={styles.pet_home_header}>
+        <h1>Adote um Pet</h1>
+        <p>Veja os detalhes de cada uma e conheça o tutor dele</p>
+      </div>
+      <div className={styles.pet_container}>
+        {pets.length > 0 &&
+          pets.map((pet) => (
+            <div className={styles.pet_card}>
+              <div
+                style={{
+                  backgroundImage: `url(${
+                    import.meta.env.VITE_API
+                  }/images/pets/${pet.images[0]})`,
+                }}
+                className={styles.pet_card_image}
+              ></div>
+              <h3>{pet.name}</h3>
+              <p>
+                <span className="bold">Peso: {pet.weight}kg</span>
+              </p>
+              {pet.available ? (
+                <Link to={`pet/${pet._id}`}>Mais detalhes</Link>
+              ) : (
+                <p className={styles.adopted_text}>Adotado!</p>
+              )}
+            </div>
+          ))}
+        {pets.length === 0 && <p>Não há pets cadastrados</p>}
+      </div>
     </section>
   );
 };
