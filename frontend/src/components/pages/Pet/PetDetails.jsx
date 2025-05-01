03b531cba2df35c3c63bd745527788d5d2484c1c
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-30 21:37:49 -0300

    Cria componente PetDetails: - Implementa exibição de detalhes do pet, incluindo nome, peso, idade e imagens - Adiciona funcionalidade para agendar visitas com autenticação via token - Integra mensagens de feedback com useFlashMessage - Exibe botão para solicitar visita ou mensagem para criar conta caso o usuário não esteja autenticado

---
+++ frontend/src/components/pages/Pet/PetDetails.jsx
@@ -0,0 +1,79 @@
+import api from "../../../utils/api";
+
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+
+import styles from "./PetDetails.module.css";
+
+import useFlashMessage from "../../../hooks/useFlashMessage";
+
+const PetDetails = () => {
+  const [pet, setPet] = useState({});
+  const { id } = useParams();
+  const { setFlashMessage } = useFlashMessage();
+  const [token] = useState(localStorage.getItem("token") || "");
+
+  useEffect(() => {
+    api.get(`/pets/${id}`).then((response) => {
+      setPet(response.data.pet);
+    });
+  }, [id]);
+
+  const schedule = async () => {
+    let msgType = "success";
+
+    const data = await api
+      .patch(`/pets/schedule/${pet._id}`, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((err) => {
+        msgType = "error";
+        return err.response.data;
+      });
+
+    setFlashMessage(data.message, msgType);
+  };
+
+  return (
+    <>
+      {pet.name && (
+        <section className={styles.pet_details_container}>
+          <div className={styles.pet_details_header}>
+            <h1>Conhecendo o Pet: {pet.name}</h1>
+            <p>Se tiver interesse, marque uma visita para conhecê-lo</p>
+          </div>
+          <div className={styles.pet_images}>
+            {pet.images.map((image, index) => (
+              <img
+                src={`${import.meta.env.VITE_API}/images/pets/${image}`}
+                alt={pet.name}
+                key={index}
+              />
+            ))}
+          </div>
+          <p>
+            <span className="bold">Peso: {pet.weigth}kg</span>
+          </p>
+          <p>
+            <span className="bold">Idade: {pet.age} anos</span>
+          </p>
+          {token ? (
+            <button onClick={schedule}>Solicitar uma visita</button>
+          ) : (
+            <p>
+              Você precisar <Link to="/register">criar uma conta</Link> para
+              solicitar a visita
+            </p>
+          )}
+        </section>
+      )}
+    </>
+  );
+};
+
+export default PetDetails;
